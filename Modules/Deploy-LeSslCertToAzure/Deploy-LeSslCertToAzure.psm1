d0e0b8844a07fa566098e18dd4d88c36f66218f6
Erik Wierschke <erik.wierschke@plus3it.com>
2018-01-17 16:26:23 -0500

    attempt to remove default request routing rulethat references removed https listener

--- Modules/Deploy-LeSslCertToAzure/Deploy-LeSslCertToAzure.psm1
+++ Modules/Deploy-LeSslCertToAzure/Deploy-LeSslCertToAzure.psm1
@@ -275,6 +275,7 @@ Function Deploy-LeSslCertToAzure() {
   
         #Create new rule for current backend Pool and created
         Write-Verbose "Adding new Routing Rule for new HTTPS Listener..."
+        Remove-AzureRmApplicationGatewayRequestRoutingRule -ApplicationGateway $appGateway -Name "rule1" -ErrorAction SilentlyContinue
         Add-AzureRmApplicationGatewayRequestRoutingRule -ApplicationGateway $appGateway -Name $appGatewayHttpsRuleName -RuleType Basic -BackendHttpSettings $poolSetting -HttpListener $listener -BackendAddressPool $backendPool
   
         Write-Verbose "Saving changes..."
